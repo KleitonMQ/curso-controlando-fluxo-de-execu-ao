d5a03461335357a019b2747934c000bde459d25c
Kleiton MQ <kleiton.x5@gmail.com>
2022-05-17 03:54:31 -0300

    aulas concluidas, esperei serem melhroes.

---
+++ Aula1/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+
+class Program
+{
+    static void Main()
+    {
+        int valor1;
+        int valor2;
+        int total;
+
+        WriteLine("Digite o primeiro valor.");
+        valor1 = int.Parse(ReadLine());
+        WriteLine("Digite o segundo valor.");
+        valor2 = int.Parse(ReadLine());
+
+        total = valor1 + valor2;
+        WriteLine($@"O primeiro valor é {valor1}, o segundo valor é {valor2}.
+A soma dos dois valores é: {total}");
+
+    }
+}
+
---
+++ Aula2/Program.cs
@@ -0,0 +1,49 @@
+﻿using static System.Console;
+
+WriteLine("Digita a sua idade");
+int idade = int.Parse(ReadLine());
+
+//sintaxe IF
+if (idade >= 18){
+    WriteLine("Você já pode ser preso.");
+}else{
+    WriteLine("Vai caçá o que fazê.");
+}
+
+
+//Sintaxe SWITCH
+switch (idade){
+    case 1:
+    //TODO Alguma coisa se tiver 1 ano.
+    break;
+    case 2:
+        //TODO Alguma coisa se tiver 2 anos.
+    break;
+default:
+    WriteLine("Mensagem padrão caso não caia em nenhuma opção acima.");
+
+break;
+}
+
+
+//sintaxe DO
+do
+{
+    //Repete esta ação enquando a condição for verdadeira.
+    //Detalhe da repetição "do" é que ela sempre vai executar pelo menos uma vez.
+    idade++;2
+    
+} while (idade < 10);
+
+//Sintaxe FOR
+for (int i = 0; i < idade; i++)
+{
+    //TODO algum calculo envolvendo a idade. 
+}
+
+//foreach é mais usado para tratar com arrays (eu acho)
+int[] lista = new int[]{1 ,2, 3 ,4};
+foreach (var item in lista)
+{
+    WriteLine(item);
+}
